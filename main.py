a410f7902b0f735ef05072425275d714a2908eed
Accolade-Qa <suraj.bhalerao@accoladeelctronics.com>
2026-03-27 17:25:53 +0530

    monor changes

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from backend.app import app
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    
+    
